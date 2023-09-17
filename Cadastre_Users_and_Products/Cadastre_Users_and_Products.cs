03d37efd52bc5dacfbe3fdee2b4d15e3b0a6d37d
Jhon Vitor <jhonsilva.vs1@gmail.com>
2023-09-17 12:38:41 -0300

    realizando alterações

--- Cadastre_Users_and_Products/Cadastre_Users_and_Products.cs
+++ Cadastre_Users_and_Products/Cadastre_Users_and_Products.cs
@@ -9,7 +9,7 @@ namespace Cadastre_User_and_Products
     public class User
     {
 
-        public string? Name { get; set; }
+        public string? Name { get; set;  }
         public string? Email { get; set; }
         public int Id { get; set; }
         public int Age { get; set; }
--- Cadastre_Users_and_Products/Program.cs
+++ Cadastre_Users_and_Products/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
 
 public class Project
 {
@@ -11,34 +12,54 @@ public class Project
     static void Main(string[] args)
     {
 
+        
 
         // Criando uma Instância da classe User
         User user = new User();
 
         // Definindo valores para as propriedades do usuário
+        int contador = 0;
+
+        while (contador < 6){
+
         Console.WriteLine("Digite seu nome: ");
         user.Name = Console.ReadLine();
+        // Validando o Nome do Usuário
+            if (user.Name == null || user.Name.Length <= 1) {
+                Console.WriteLine("Nome Inválido!");
+                break;
+            }
 
 
-        Console.WriteLine($"Seu ID é: {user.Id}");
+            Console.WriteLine("Informe a sua idade: ");
+            string? AgeString = Console.ReadLine();
+            string? ageString = AgeString;
+            user.Agestring = ageString;
 
 
-        Console.WriteLine("Informe a sua idade: ");
-        string? AgeString = Console.ReadLine();
-        string? ageString = AgeString;
-        user.Agestring = ageString;
+            Console.WriteLine("Digite seu E-mail: ");
+            user.Email = Console.ReadLine();
+            if (user.Email == null || user.Email.Length == 0)
+            {
+                Console.WriteLine("Email Inválido!");
+                break;
+            }
 
+            Console.WriteLine("Usuário Ativo?");
+            user.active = true;
+            if (user.active == false ) 
+            {
+                Console.WriteLine("Usuário Inativo. Não cadastrado.");
+                break;
+            };
+            Console.WriteLine($"{user.active}");
 
-        Console.WriteLine("Digite seu E-mail: ");
-        user.Email = Console.ReadLine();
+            Console.WriteLine($"Seu ID é: {user.Id}");
 
-
-        Console.WriteLine("Usuário Ativo?");
-        user.active = true;
-        Console.WriteLine($"{user.active}");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Usuário Cadastrado com sucesso");
+            Console.WriteLine("-------------------------------");
+            contador++;
+        }
     }
 }
-
-
-
-
